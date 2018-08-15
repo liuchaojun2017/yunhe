38a387067a19154e38b3c6b9e9917c10e0e204e4
chaojun2017 <15538395097@163.com>
2018-08-15 10:21:32 +0800

    第一次测试

---
+++ 20180814/src/com/yunhe/test/test.java
@@ -0,0 +1,15 @@
+package com.yunhe.test;
+
+public class test {
+	public static void main(String[] args) {
+		String s="abc";
+		byte b=(byte) 300;
+		System.out.println(b);
+		
+		char c='a';
+		int a=1;
+		System.out.println(c+a);
+		
+		
+	}
+}
